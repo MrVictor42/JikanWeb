f67d7552b7ec4c22a5fa9df69459c4fc1ed847d8
MrVictor42 <guin-hu@hotmail.com>
2021-08-05 14:32:24 -0300

    Testes de acessibilidade

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -6,6 +6,14 @@ import AnimeListDay from '../anime/AnimeListDay';
 const Homepage = () => {
     return (
         <>
+            <h1 style = {{ color : "#ff0000" }}>
+                <b aria-label="Temos um crédito consignado pré-aprovado com o valor de R$ 42kk"> 
+                    Temos um crédito consignado pré-aprovado com o valor de R$ 42kk 
+                </b>
+            </h1>
+            <p aria-hidden="true" style = {{ color : "#ff0000" }}>
+                Confira as condições ai
+            </p>
             <AnimeListDay />
             <AnimeList />
         </>
